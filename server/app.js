96bc50c844e6430b85dc27f6cea0d6f261a08697
if1live <libsora25@gmail.com>
2015-04-26 01:24:09 +0900

    socket.io + html/unity

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 // constants
 var HTTP_PORT = 3000;
-var GAME_PORT = 8124;
 
 // code start
 var express = require('express')
@@ -12,6 +11,7 @@ var io = require('socket.io')(http);
 app.set('port', HTTP_PORT);
 app.use(express.static(__dirname + '/publish'));
 
+// TODO Game World
 var g_count = 0;
 
 //app.get('/', function(req, res) {
@@ -25,38 +25,18 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 
   socket.on('chat message', function(msg) {
     console.log('message : ' + msg);
-    io.emit('chat message', msg);
+    io.emit('chat message', {"msg": msg});
   });
 });
-
 
 
 http.listen(app.get('port'), function() {
   console.log('http listening on *:' + app.get('port'));
 });
-
-
-/*
-var net = require('net');
-
-var server = net.createServer(function(c) { //'connection' listener
-  console.log('client connected');
-  g_count += 1;
-  console.log('counter : ' + g_count);
-  c.on('end', function() {
-    console.log('client disconnected');
-  });
-  c.write('hello\r\n');
-  c.pipe(c);
-});
-
-server.listen(GAME_PORT, function() { //'listening' listener
-  console.log('server bound');
-});
-*/
