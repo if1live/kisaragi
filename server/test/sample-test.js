46703b282b4c899ba796897d999c68d67e7771b5
if1live <libsora25@gmail.com>
2015-04-26 12:44:33 +0900

    add sample unit test

---
+++ server/test/sample-test.js
@@ -0,0 +1,12 @@
+var vows = require('vows');
+var assert = require('assert');
+
+vows.describe('sample').addBatch({
+  'foo': {
+    topic: function() { return 1/0 },
+
+    'infinity': function(topic) {
+      assert.equal(topic, Infinity);
+    }
+  }
+}).run();
