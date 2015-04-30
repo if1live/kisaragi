6e77586a41ae4a9ebf4c28ba59733aab72da9439
if1live <libsora25@gmail.com>
2015-04-30 22:02:28 +0900

    require.js 일단 제거. 이거 쓰면 콘솔 디버깅이 어려워진다(블럭 밖이라서 접근 못함) 좌표계는 xy + 왼쪽아래 원점

--- static/index.js
+++ static/index.js
@@ -1,102 +1,119 @@
-require([
-  'underscore', 
-  'jquery', 
-  'socket.io', 
-  'game.js'
-  ], function(_, $, io, game) {
-  console.log(game);
-  // game library test
-  (new game.User()).foo();
+// command
+$('.cmd-move').click(function() {
+  var dx = parseInt($(this).data('dx'), 10);
+  var dy = parseInt($(this).data('dy'), 10);
   
-  // network & handler
-  var socket = io();
-  
-  function dumpCommunication(cmd, obj) {
-    console.log(cmd + " : " + JSON.stringify(obj));
+  var filtered = _.filter(users, function(obj) {
+    return obj.id == currUserId;
+  });
+  if(filtered.length > 0) {
+    var user = filtered[0];
+
+    var x = user.x + dx;
+    var y = user.y + dy;
+    requestMove(x, y);
   }
-  
-  socket.on('login', function(obj) {
-    dumpCommunication('login', obj);
-  
-    socket.emit('requestMap', {});
-    socket.emit('requestUserList', {});
-  });
-  
-  socket.on('requestMap', function(obj) {
-    var html = '<table><tbody>' + _.reduce(obj.data, function(memo, row) {
-      return memo + '<tr>' + _.reduce(row, function(memo, cell) {
-        return memo + '<td>–</td>';
-      }, '') + '</tr>';
-    }, '') + '</tbody></table>';
-    
-    $("#game").html(html);
-  //  dumpCommunication('requestMap', obj);
-  });
-  
-  var users = new Object();
-  
-  socket.on('moveOccur', function(obj) {
-    _.each(users, function(user, i) {
-      user.valid = false;
-    });
-    
-    _.each(obj.user_list, function(user, i) {
-      var x = user.pos[0];
-      var y = user.pos[1];
-      
-      if(users[user.id]) {
-        $('#game > table > tbody > tr:nth-child(' + (users[user.id].x + 1) + ') > td:nth-child(' + (users[user.id].y + 1) + ')').html('–');
-      }
-      users[user.id] = { 'id': user.id, 'x': x, 'y': y, 'valid': true };
-    });
-    
-    _.each(users, function(user, i) {
-      if(user.valid) {
-        $('#game > table > tbody > tr:nth-child(' + (user.x + 1) + ') > td:nth-child(' + (user.y + 1) + ')').html(user.id);
-      }
-      else {
-        delete users[i];
-      }
-    });
-    
-    dumpCommunication('moveOccur', obj);
-  });
-  
-  socket.on('ping', function(obj) {
-    var now = Date.now();
-    var prev = obj.timestamp;
-    var diff = now - prev;
-    console.log("ping : " + diff + "ms");
-  });
-  
-  function ping() {
-    var timestamp = Date.now();
-    socket.emit('ping', {timestamp:timestamp});
-  }
-  
-  socket.on('echo', function(ctx) {
-    dumpCommunication('echo', ctx);
-  });
-  
-  function echo(ctx) {
-    socket.emit('echo', ctx);
-  }
-  
-  socket.on('echo_all', function(ctx) {
-    dumpCommunication('echo_all', ctx);
-  });
-  
-  function echo_all(ctx) {
-    socket.emit('echo_all', ctx);
-  }
-  
-  function requestMove(y, x) {
-    socket.emit('requestMove', {
-      x: x,
-      y: y
-    });
-  }
-  
-  // main
-  ping();
 });
+
+// network & handler
+var socket = io();
+
+function dumpCommunication(cmd, obj) {
+  console.log(cmd + " : " + JSON.stringify(obj));
+}
+
+var currUserId = null;
+
+socket.on('login', function(obj) {
+  dumpCommunication('login', obj);
+  
+  currUserId = obj.id;
+
+  socket.emit('requestMap', {});
+  socket.emit('requestUserList', {});
+});
+
+var mapWidth = null;
+var mapHeight = null;
+
+socket.on('requestMap', function(obj) {
+  mapWidth = obj.width;
+  mapHeight = obj.height;
+  
+  var html = '<table><tbody>' + _.reduce(obj.data, function(memo, row) {
+    return memo + '<tr>' + _.reduce(row, function(memo, cell) {
+      return memo + '<td>–</td>';
+    }, '') + '</tr>';
+  }, '') + '</tbody></table>';
+  
+  $("#game").html(html);
+//  dumpCommunication('requestMap', obj);
+});
+
+var users = new Object();
+
+socket.on('moveOccur', function(obj) {
+  _.each(users, function(user, i) {
+    user.valid = false;
+  });
+  
+  _.each(obj.user_list, function(user, i) {
+    console.log(user.pos);
+    var x = user.pos[0];
+    var y = user.pos[1];
+    
+    if(users[user.id]) {
+      $('#game > table > tbody > tr:nth-child(' + (mapHeight - users[user.id].y) + ') > td:nth-child(' + (users[user.id].x + 1) + ')').html('–');
+    }
+    users[user.id] = { 'id': user.id, 'x': x, 'y': y, 'valid': true };
+  });
+  
+  _.each(users, function(user, i) {
+    if(user.valid) {
+      $('#game > table > tbody > tr:nth-child(' + (mapHeight - user.y) + ') > td:nth-child(' + (user.x + 1) + ')').html(user.id);
+    }
+    else {
+      delete users[i];
+    }
+  });
+  
+  dumpCommunication('moveOccur', obj);
+});
+
+socket.on('ping', function(obj) {
+  var now = Date.now();
+  var prev = obj.timestamp;
+  var diff = now - prev;
+  console.log("ping : " + diff + "ms");
+});
+
+function ping() {
+  var timestamp = Date.now();
+  socket.emit('ping', {timestamp:timestamp});
+}
+
+socket.on('echo', function(ctx) {
+  dumpCommunication('echo', ctx);
+});
+
+function echo(ctx) {
+  socket.emit('echo', ctx);
+}
+
+socket.on('echo_all', function(ctx) {
+  dumpCommunication('echo_all', ctx);
+});
+
+function echo_all(ctx) {
+  socket.emit('echo_all', ctx);
+}
+
+function requestMove(x, y) {
+  socket.emit('requestMove', {
+    x: x,
+    y: y
+  });
+}
+
+// main
+ping();
