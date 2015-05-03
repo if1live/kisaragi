48892466e71eb62b19e2e1a028cbecebc06583a2
if1live <libsora25@gmail.com>
2015-05-03 17:35:02 +0900

    직렬화 라이브러리 분리. 이동 검사의 버그 수정

--- test/base-test.js
+++ test/base-test.js
@@ -10,5 +10,17 @@ vows.describe('base').addBatch({
     'success': function(topic) {
       assert.equal(topic, true);
     }
+  },
+  'Serializer': {
+    topic: new base.Serializer(['id', 'pos'], {id: 1, pos: [2, 3], category: 'user'}),
+    'serialize': function(topic) {
+      var serialized = topic.serialize();
+      assert.deepEqual(serialized, {id: 1, pos: [2, 3]});
+    },
+    'deserialize': function(topic) {
+      topic.deserialize({id: 2, pos: [3, 4]});
+      assert.equal(topic.obj.id, 2);
+      assert.deepEqual(topic.obj.pos, [3, 4]);
+    }
   }
 }).export(module);
