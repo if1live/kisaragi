9c793c194b98b4a46da27846812f5e17499e3ace
if1live <libsora25@gmail.com>
2015-05-28 22:36:07 +0900

    remove unused code

--- test/network/ping_spec.js
+++
@@ -1,7 +0,0 @@
-// Ŭnicode please
-///<reference path="../test.d.ts"/>
-if (typeof module !== 'undefined') {
-    var kisaragi = require('../../app/kisaragi');
-}
-describe('ClientPing', function () {
-});
